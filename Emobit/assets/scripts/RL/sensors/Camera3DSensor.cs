914af04cbb9478d6f7c4aca533f5d160a5f935e3
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2024-04-18 15:51:53 -0300

    sync with ai4urepo

--- Emobit/assets/scripts/RL/sensors/Camera3DSensor.cs
+++ Emobit/assets/scripts/RL/sensors/Camera3DSensor.cs
@@ -4,15 +4,24 @@ using System;
 namespace ai4u;
 
 /// <summary>
-/// This sensor get a screenshot from the viewport.
+/// This sensor get a screenshot from a Viewport object. 
+/// Rendered image can be projected on a Subviewport if set <code>viewport</code> field 
+/// with a valid Subviewport object. 
 /// </summary>
-public partial class ScreenSensor : Sensor
+public partial class Camera3DSensor : Sensor
 {
+
+    ///<summary
+    /// Source camera witch the image will be captured.
+    ///</summary>
+    [Export]
+    private Camera3D camera;
+
 
     /// <summary>
     /// The <code>grayScale</code> field defines the format of the generated image.If true, the image format is L8,
     /// containing only the luminance of the captured image from the viewport.
-    /// Otherwise, the format is RGB8.Prefer grayscale images for performance reasons.
+    /// Otherwise, the format is RGB8. Prefer grayscale images for performance reasons.
     /// </summary>
     [Export]
     private bool grayScale = true;
@@ -29,13 +38,28 @@ public partial class ScreenSensor : Sensor
     [Export]
     private int height = 61;
 
+    
+    /// <summary>
+    /// The viewport to rendering. If null, it use default viewport.
+    /// </summary>
+    [Export]
+    private SubViewport viewport;
+
     private Image currentImg = null;
+
     private MaxSizeQueue<string> history;
 
     public override void OnSetup(Agent agent)
     {
-        MakeData();
         this.agent = (BasicAgent)agent;
+        if (viewport != null)
+        {
+            this.viewport.Size = new Vector2I(width, height);
+            this.viewport.World3D = GetViewport().World3D;
+            RenderingServer.ViewportAttachCamera(viewport.GetViewportRid(), camera.GetCameraRid());
+        }
+
+        MakeData();
     }
 
     private void MakeData()
@@ -60,11 +84,17 @@ public partial class ScreenSensor : Sensor
 
     public override string[] GetStringValues()
     {
-
         if (agent != null && !agent.Done)
         {
-            currentImg = GetViewport().GetTexture().GetImage();
-            
+            if (viewport != null)
+            {
+                currentImg = viewport.GetTexture().GetImage();
+            }
+            else
+            {
+                currentImg = camera.GetViewport().GetTexture().GetImage();
+            }
+
             if (grayScale)
             {
                 currentImg.Convert(Image.Format.L8);
